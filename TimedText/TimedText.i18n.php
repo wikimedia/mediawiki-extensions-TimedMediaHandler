61235949817a9597b80bb788cf4cb434ce435977
Michael Dale <dale@users.mediawiki.org>
2010-05-12 20:02:51 +0000

    added TimedMediaHandler ( js2 oggHandler ) hosted javascript module ( no more svn:externals )

---
+++ TimedText/TimedText.i18n.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Internationalisation for TimedText
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+$messages['en'] = array(
+	'mew-timedtext-editor' => 'Timed text editor',
+	'mwe-stage-transcribe' => 'Transcribe',
+	'mwe-stage-sync' => 'Sync',
+	'mwe-stage-translate' => 'Translate',
+	'mwe-stage-upload' => 'Upload from local file',
+	'mwe-select-language' => 'Select language',
+	'mwe-file-language' => 'Subtitle file language',
+	'mwe-upload-text' => 'Upload text file',
+	'mwe-uploading-text' => 'Uploading text file',
+	'mwe-upload-text-desc-title' => 'Upload a text file',
+	'mwe-upload-text-desc-help' => 'The upload text file interface accepts .srt files',
+	'mwe-upload-text-desc-help-browse' => 'Browse your local computer for the srt file you want to upload',
+	'mwe-upload-text-desc-help-select' => 'Select the language of the file',
+	'mwe-upload-text-desc-help-review' => 'Review / edit the text content and then press upload to add the text',
+	'mwe-upload-text-preview' => 'Review text',
+	'mwe-upload-text-success' => 'Upload of timed text was successful',
+	'mwe-upload-text-done' => 'Upload done',
+	'mwe-upload-text-fail-desc' => 'Upload was unsuccessful',
+	'mwe-upload-text-fail-title' => 'Upload failed',
+	'mwe-upload-text-another' => 'Upload another',
+	'mwe-upload-text-done-uploading' => 'Done uploading',
+	'mwe-back-btn' => 'Back',
+	'mwe-chose-text' => 'Chose text',
+	'mwe-add-timed-text' => 'Add timed text',
+	'mwe-loading-text-edit' => 'Loading timed text editor',
+	'mwe-search' => 'Search clip',
+	'mwe-layout' => 'Layout',
+	'mwe-layout-ontop' => 'Ontop of video',
+	'mwe-layout-below' => 'Below video',
+	'mwe-layout-off' => 'Hide subtitles',
+	'mwe-loading-text' => 'Loading text ...',
+	'mwe-key-language' => '$1, $2',
+	'mwe-textcat-cc' => 'Captions',
+	'mwe-textcat-sub' => 'Subtitles',
+	'mwe-textcat-tad' => 'Audio description',
+	'mwe-textcat-ktv' => 'Karaoke',
+	'mwe-textcat-tik' => 'Ticker text',
+	'mwe-textcat-ar' => 'Active regions',
+	'mwe-textcat-nb' => 'Annotation',
+	'mwe-textcat-meta' => 'Timed metadata',
+	'mwe-textcat-trx' => 'Transcript',
+	'mwe-textcat-lrc' => 'Lyrics',
+	'mwe-textcat-lin' => 'Linguistic markup',
+	'mwe-textcat-cue' => 'Cue points',
+	'mwe-language-subtitles-for-clip' => '$1 subtitles for clip: $2',
+	'mwe-language-no-subtitles-for-clip' => 'No $1 subtitles where found for clip: $2',
+);
