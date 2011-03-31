4f46267f6d05b5a8357652a3c497b211a2b6e660
Michael Dale <dale@users.mediawiki.org>
2011-03-31 20:20:45 +0000

    moved test files to tests keeps with convention

---
+++ tests/test/phpunit/WebVideoApiVideoInfoTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once dirname(__FILE__) . '/commandLine.inc';
+
+/**
+ * TMHApiVideoInfoTest test case.
+ * 
+ * @ingroup timedmedia
+ * @since 0.2
+ * @author Michael Dale
+ */
+class TMHApiVideoInfoTest extends PHPUnit_Framework_TestCase {
+	
+	
+}
