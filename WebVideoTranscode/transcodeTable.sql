3ef1680fabaed042b35de36ee5c58b3142b51350
Michael Dale <dale@users.mediawiki.org>
2011-05-13 05:32:44 +0000

    removed tableOptions

--- WebVideoTranscode/transcodeTable.sql
+++ WebVideoTranscode/transcodeTable.sql
@@ -10,6 +10,6 @@ CREATE TABLE /*$wgDBprefix*/transcode (
 	transcode_time_startwork VARBINARY( 14 ) NULL,
 	transcode_time_success VARBINARY( 14 ) NULL,
 	transcode_time_error VARBINARY( 14 ) NULL,
-) /*$wgDBTableOptions*/;
+);
 CREATE INDEX ( transcode_image_name , transcode_state , transcode_key ),
 CREATE INDEX ( transcode_time_addjob ,transcode_time_startwork , transcode_time_success, transcode_time_error ),
