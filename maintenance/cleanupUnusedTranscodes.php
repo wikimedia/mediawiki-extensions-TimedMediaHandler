f2e3759925628e1ac9125fcac9e952d3ef1b7dec
Jan Gerber <j@thing.net>
2012-07-27 09:40:20 +0000

    add option to remove all encodes with one key
    Change-Id: I00da4bc61d9ad59f006ff6ce6362372a5c2f6405

--- maintenance/cleanupUnusedTranscodes.php
+++ maintenance/cleanupUnusedTranscodes.php
@@ -10,17 +10,22 @@ if ( $IP === false ) {
 require_once( "$IP/maintenance/Maintenance.php" );
 
 class CleanupUnusedTranscodes extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( "key", "remove all transcodes for given key", false, true );
+	}
 	public function execute() {
 		global $wgEnabledTranscodeSet;
 
+		if ( $this->hasOption( "key" ) ) {
+			$where = array( 'transcode_key' =>  $this->getOption( 'key' ) ),
+		} else {
+			$where = 'transcode_key NOT IN ("'. implode('", "', $wgEnabledTranscodeSet ).'")';
+		}
 		$this->output( "Cleanup unused transcodes:\n" );
 		$dbr = wfGetDB( DB_SLAVE );
-		$res = $dbr->select(
-			'transcode',
-			'*',
-			'transcode_key NOT IN ("'. implode('", "', $wgEnabledTranscodeSet ).'")',
-			__METHOD__
-		);
+		$res = $dbr->select( 'transcode', '*', $where, __METHOD__ );
 		foreach ( $res as $row ) {
 			$this->output(
 				'remove: '. $row->transcode_image_name . ' ' . $row->transcode_key . "\n"
