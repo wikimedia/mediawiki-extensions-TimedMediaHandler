6ab6cc0154b0ab1a7e8dbcf254feb380dbfc8102
Jan Gerber <j@thing.net>
2012-07-31 00:48:27 +0000

    load data-title from sources
    fix titles in download panel
    
    Change-Id: I53c69e355cd4bf79cfe6f9fcf6fad8ce7c1db65e

--- MwEmbedModules/EmbedPlayer/resources/mw.MediaSource.js
+++ MwEmbedModules/EmbedPlayer/resources/mw.MediaSource.js
@@ -52,6 +52,9 @@ mw.mergeConfig( 'EmbedPlayer.SourceAttributes', [
 	'data-flavorid', // a source flavor id ( useful for targeting devices )
 	'data-aspect', // the aspect ratio, useful for adaptive protocal urls that don't have a strict height / width
 
+	// Used as title in download panel
+	'data-title',
+
 	// Used for download attribute on mediawiki
 	'data-mwtitle',
 	// used for setting the api provider for mediawiki
