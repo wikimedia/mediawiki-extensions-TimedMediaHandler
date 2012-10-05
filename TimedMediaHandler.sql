97923931b697ec05b338b8876a47c1e536cd55f5
Aaron Schulz <aschulz@wikimedia.org>
2012-10-04 17:46:13 -0700

    Fix for SQLite DBs.
    Change-Id: Ic63db2131dfb83da10f9f65767e81d6238516bb8

--- TimedMediaHandler.sql
+++ TimedMediaHandler.sql
@@ -2,7 +2,7 @@
 -- 2011-04-05  <mdale@wikimedia.org>
 --
 CREATE TABLE /*_*/transcode (
-	transcode_id INT NOT NULL AUTO_INCREMENT,
+	transcode_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	transcode_image_name VARCHAR(255) NOT NULL,
 	transcode_key VARCHAR(48) NOT NULL,
 	transcode_error longtext NOT NULL,
@@ -10,8 +10,7 @@ CREATE TABLE /*_*/transcode (
 	transcode_time_startwork VARCHAR(14) NULL,
 	transcode_time_success VARCHAR(14) NULL,
 	transcode_time_error VARCHAR(14) NULL,
-	transcode_final_bitrate INT NOT NULL,
-	PRIMARY KEY (`transcode_id`)
+	transcode_final_bitrate INT NOT NULL
 ) /*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/transcode_name_inx ON /*_*/transcode( transcode_image_name, transcode_key );
