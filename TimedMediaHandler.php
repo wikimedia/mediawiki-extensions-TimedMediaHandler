paladox/testrepo-tm

Allow registered users to reset transcodes
resetting transcodes is like an expensive purge,
this should be available to normal users, that
way users can retry failed encodes in case
the error was transient.

Change-Id: Ide9505fdcea13b96b55372a79ee588cc4c63a00e

TimedMediaHandler.php
@@ -48,6 +48,8 @@ $wgMwEmbedModuleConfig['TimedText.ShowAddTextLink'] = true;
 
 // Which users can restart failed or expired transcode jobs:
 $wgGroupPermissions['sysop']['transcode-reset'] = true;
+$wgGroupPermissions['user']['transcode-reset'] = true;
+
 // Which users can see Special:TimedMediaHandler
 $wgGroupPermissions['sysop']['transcode-status'] = true;
 

wikimedia/mediawiki-extensions-TimedMediaHandler

TimedMediaHandler.php
@@ -48,6 +48,8 @@ $wgMwEmbedModuleConfig['TimedText.ShowAddTextLink'] = true;
 
 // Which users can restart failed or expired transcode jobs:
 $wgGroupPermissions['sysop']['transcode-reset'] = true;
+$wgGroupPermissions['autoconfirmed']['transcode-reset'] = true;
+
 // Which users can see Special:TimedMediaHandler
 $wgGroupPermissions['sysop']['transcode-status'] = true;
 