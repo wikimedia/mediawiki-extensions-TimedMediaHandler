f36d34309996086d414c8ee3e916003bc7e60cdb
Derk-Jan Hartman <hartman.wiki@gmail.com>
2014-11-15 14:56:55 +0100

    JSCS cleanup for timed media handler
    Change-Id: Ia6b22c951e33e4f1fdfbf57cd5562f125dc5f656

--- resources/mw.PopUpThumbVideo.js
+++ resources/mw.PopUpThumbVideo.js
@@ -1,32 +1,32 @@
 /**
 * Simple script to add pop-up video dialog link support for video thumbnails
 */
-( function( mw, $ ) {
-	$(document).ready(function(){
-		$('.PopUpMediaTransform a').each( function(){
-			var parent = $(this).parent();
+( function ( mw, $ ) {
+	$( document ).ready( function () {
+		$('.PopUpMediaTransform a').each( function () {
+			var parent = $( this ).parent();
 			if ( parent.attr( 'videopayload' ) ) {
-				$( this ).click( function( event ){
+				$( this ).click( function ( event ) {
 					var thisref = this;
 
 					mw.loader.using( 'mw.MwEmbedSupport', function () {
 						var $videoContainer = $( $( thisref ).parent().attr( 'videopayload' ) );
 						mw.addDialog({
-							'width' : 'auto',
-							'height' : 'auto',
-							'title' : mw.html.escape( $videoContainer.find('video,audio').attr('data-mwtitle') ),
-							'content' : $videoContainer,
-							'close' : function(){
-								// On close pause the video on close ( so that playback does not continue )
-								var domEl = $( this ).find( 'video,audio' ).get(0);
-								if( domEl && domEl.pause ) {
+							'width': 'auto',
+							'height': 'auto',
+							'title': mw.html.escape( $videoContainer.find( 'video, audio' ).attr( 'data-mwtitle' ) ),
+							'content': $videoContainer,
+							'close': function () {
+								// On close pause the video ( so that playback does not continue )
+								var domEl = $( this ).find( 'video, audio' ).get( 0 );
+								if ( domEl && domEl.pause ) {
 									domEl.pause();
 								}
 								return true;
 							}
 						})
-						.css('overflow', 'hidden')
-						.find('video,audio').embedPlayer();
+						.css( 'overflow', 'hidden' )
+						.find( 'video, audio' ).embedPlayer();
 					} );
 					// don't follow file link
 					return false;
@@ -39,5 +39,5 @@
 				}
 			} /* else fall back to linking directly to media file */
 		} );
-	});
+	} );
 } )( mediaWiki, jQuery );
