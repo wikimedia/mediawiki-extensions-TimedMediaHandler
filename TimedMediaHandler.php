d6dbc4c4c14ab14651a42a54d9cc856f15090705
paladox <thomasmulhall410@yahoo.com>
2015-08-07 11:40:06 +0100

    Add license name to main php file
    Change-Id: Id553e5b3b417b77687903e459383cd2025e95afa

--- TimedMediaHandler.php
+++ TimedMediaHandler.php
@@ -299,4 +299,5 @@ $wgExtensionCredits['media'][] = array(
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:TimedMediaHandler',
 	'descriptionmsg' => 'timedmedia-desc',
 	'version'        => '0.4.0',
+	'license-name'   => 'GPL-2.0+',
 );
