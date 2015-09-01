3067ca02c16fbc2f10eb3f5f62c7d3569186b448
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-09-01 21:27:07 +0200

    Add a TimedText namespace tab for video files
    This adds an extra TimedText namespace tab to files that qualify
    having TimedText. This would make the TimedText feature more
    discoverable.
    
    Bug: T116365
    Change-Id: Ia7a378fbbece63b118b5a9356092ff6c3fac192d

--- TimedMediaHandler.hooks.php
+++ TimedMediaHandler.hooks.php
@@ -334,6 +334,8 @@ class TimedMediaHandlerHooks {
 			// Check for timed text page:
 			$wgHooks[ 'ArticleFromTitle' ][] = 'TimedMediaHandlerHooks::checkForTimedTextPage';
 			$wgHooks[ 'ArticleContentOnDiff' ][] = 'TimedMediaHandlerHooks::checkForTimedTextDiff';
+
+			$wgHooks[ 'SkinTemplateNavigation' ][] = 'TimedMediaHandlerHooks::onSkinTemplateNavigation';
 		} else {
 			// overwrite TimedText.ShowInterface for video with mw-provider=local
 			$wgMwEmbedModuleConfig['TimedText.ShowInterface.local'] = 'off';
@@ -426,6 +428,17 @@ class TimedMediaHandlerHooks {
 		return true;
 	}
 
+	public static function onSkinTemplateNavigation( SkinTemplate &$sktemplate, array &$links ) {
+		if ( self::isTimedMediaHandlerTitle( $sktemplate->getTitle() ) ) {
+			$ttTitle = Title::makeTitleSafe( NS_TIMEDTEXT, $sktemplate->getTitle()->getDBkey() );
+			if ( !$ttTitle ) {
+				return;
+			}
+			$links[ 'namespaces' ][ 'timedtext' ] =
+				$sktemplate->tabAction( $ttTitle, 'timedtext', false, '', false );
+		}
+	}
+
 	/**
 	 * Wraps the isTranscodableFile function
 	 * @param $title Title
@@ -477,6 +490,22 @@ class TimedMediaHandlerHooks {
 			return true;
 		}
 		return false;
+	}
+
+	public static function isTimedMediaHandlerTitle( $title ) {
+		if ( !$title->inNamespace( NS_FILE ) ) {
+			return false;
+		}
+		$file = wfFindFile( $title );
+		// Can't find file
+		if ( !$file ) {
+			return false;
+		}
+		$handler = $file->getHandler();
+		if ( !$handler ) {
+			return false;
+		}
+		return $handler instanceof TimedMediaHandler;
 	}
 
 	/**
