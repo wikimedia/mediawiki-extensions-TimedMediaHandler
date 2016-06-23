98ec9ec54e7f53feb81e59e799839176d0d593dd
Derk-Jan Hartman <hartman.wiki@gmail.com>
2016-06-23 11:21:49 +0200

    TMH: Remove old commented code of video.js player
    Change-Id: I8cf99d24274b3911bdc03f1276617929057db786

--- resources/ext.tmh.player.js
+++ resources/ext.tmh.player.js
@@ -37,56 +37,6 @@
 		}
 	};
 
-	/*
-	function growPlayer() {
-		// FIXME: fancy transform stuff should be added here :)
-		// Possibly only do this when content renderer sets a certain class ?
-		var targetWidth = 600,
-			player = this,
-			$playerDiv = $( player.el() );
-
-		var width = player.width();
-		var enlargeRatio = targetWidth / width;
-		if ( width < targetWidth ) {
-			$playerDiv
-			.addClass( 'vjs-expanded' )
-			.data( {
-				'original-height': player.height(),
-				'original-width': width,
-				style: $playerDiv.attr( 'style' )
-			} );
-			player.height( Math.ceil( player.height() * enlargeRatio ) );
-			player.width( targetWidth );
-			var parentThumb = $playerDiv.closest( '.thumb' );
-			if ( parentThumb.length ) {
-				parentThumb.data( {
-					style: parentThumb.attr( 'style' ),
-					'class': parentThumb.attr( 'class' )
-				} );
-				parentThumb.removeClass( 'thumb tright tleft tnone' ).addClass( 'removedThumb' ).css( { clear: 'both' } );
-				parentThumb.children( '.thumbinner' ).removeClass( 'thumbinner' ).addClass( 'removedInner' ).css( { width: 'auto', height: 'auto', 'margin-left': 'auto', 'margin-right': 'auto' } );
-			}
-			$playerDiv.css( { clear: 'both', 'margin-left': 'auto', 'margin-right': 'auto' } );
-		}
-
-	}
-
-	function shrinkPlayer() {
-		var player = this,
-			$playerDiv = $( player.el() );
-		if ( $playerDiv.hasClass( 'vjs-expanded' ) ) {
-			player.width( $playerDiv.data( 'original-width' ) );
-			player.height( $playerDiv.data( 'original-height' ) );
-			$playerDiv.removeClass( 'vjs-expanded' ).attr( { style: $playerDiv.data( 'style' ) } );
-			var parentThumb = $playerDiv.closest( '.removedThumb' );
-			if ( parentThumb.length ) {
-				parentThumb.attr( { style: parentThumb.data( 'style' ), 'class': parentThumb.data( 'class' ) } );
-				parentThumb.children( '.removedInner' ).attr( 'class', 'thumbinner' );
-			}
-		}
-	}
-	*/
-
 	/**
 	 * Load video players for a jQuery collection
      */
@@ -115,9 +65,6 @@
 
 			// Launch the player
 			videojs( videoplayer, playerConfig ).ready( function () {
-				// TODO disabled the 'enlarging' for now. reconsidering alternative presentation modes
-				// this.on( 'play', growPlayer );
-				// this.on( 'ended', shrinkPlayer );
 				/* More custom stuff goes here */
 			} );
 		} );
