1a36fd1bdddbadb7abca5c549034a713151bb99c
Brion Vibber <brion@pobox.com>
2017-01-11 11:02:57 -0800

    TMH job queue split into low and high priority
    This is a first step to retooling the queue management for TMH transcode.
    
    Lower resolutions and shorter files are now transcoded via one queue
    ('webVideoTranscodePrioritized' jobs) while high resolutions and long
    files are transcoded in a second (the existing 'webVideoTranscode' job)
    
    Beware that in production, this may require setting up the new queue
    to send to the scalers!
    
    Does not yet address relative prioritization after upload; each queue
    can still bog down if it gets behind.
    
    The thresholds can be configured via global vars:
    $wgTmhPriorityResolutionThreshold = 480; // pixels height
    $wgTmhPriorityLengthThreshold = 900; // seconds
    
    Bug: T155098
    Depends-On: I3aee49682f66cdd7c7a6b9267d8d486affc2903a
    Depends-On: I83e5dab7dfb7be7a6df1a516701ef534601d530f
    Change-Id: I3d834adddfa68e91bd5369246724cac2360f0de6

--- WebVideoTranscode/WebVideoTranscode.php
+++ WebVideoTranscode/WebVideoTranscode.php
@@ -1153,6 +1153,8 @@ class WebVideoTranscode {
 	 * @param $transcodeKey String transcode key
 	 */
 	public static function updateJobQueue( &$file, $transcodeKey ) {
+		global $wgTmhPriorityResolutionThreshold, $wgTmhPriorityLengthThreshold;
+
 		$fileName = $file->getTitle()->getDbKey();
 		$db = $file->repo->getMasterDB();
 
@@ -1182,9 +1184,19 @@ class WebVideoTranscode {
 				return;
 			}
 
+			// Set the priority
+			$transcodeHeight = 0;
+			$matches = [];
+			if ( preg_match( '/^(\d+)p/', $transcodeKey, $matches ) ) {
+				$transcodeHeight = intval( $matches[0] );
+			}
+			$prioritized = ( $transcodeHeight <= $wgTmhPriorityResolutionThreshold )
+				&& ( $file->getLength() <= $wgTmhPriorityLengthThreshold );
+
 			$job = new WebVideoTranscodeJob( $file->getTitle(), [
 				'transcodeMode' => 'derivative',
 				'transcodeKey' => $transcodeKey,
+				'prioritized' => $prioritized
 			] );
 
 			try {
