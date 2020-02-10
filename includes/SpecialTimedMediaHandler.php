3dcf26abc2f62a849ff7fa3a418f65611906034e
Derk-Jan Hartman <hartman.wiki@gmail.com>
2020-02-10 22:39:34 +0100

    Special:TimedMediaHandler remove stats of originals
    Remove the statistics on originals in Special:TimedMediaHandler
    The list was incomplete (missing MP4) and Special:MediaStatistics
    provides a better overview these days.
    
    Bug: T240837
    Change-Id: If5e74b8e4f79713af94e1230f743501a036e4161

--- includes/SpecialTimedMediaHandler.php
+++ includes/SpecialTimedMediaHandler.php
@@ -16,16 +16,6 @@ class SpecialTimedMediaHandler extends SpecialPage {
 		'queued' => 'transcode_time_addjob IS NOT NULL AND transcode_time_startwork IS NULL',
 		'missing' => 'transcode_time_addjob IS NULL',
 	];
-	private $formats = [
-		'ogg' => 'img_major_mime="application" AND img_minor_mime = "ogg"',
-		'webm' => 'img_major_mime="video" AND img_minor_mime = "webm"',
-	];
-	private $audioFormats = [
-		'ogg' => 'img_major_mime="application" AND img_minor_mime = "ogg"',
-		'webm' => 'img_major_mime="audio" AND img_minor_mime = "webm"',
-		'flac' => 'img_major_mime="audio" AND img_minor_mime="x-flac"',
-		'wav' => 'img_major_mime="audio" AND img_minor_mime="wav"',
-	];
 
 	public function __construct( $request = null, $par = null ) {
 		parent::__construct( 'TimedMediaHandler', 'transcode-status' );
@@ -36,28 +26,6 @@ class SpecialTimedMediaHandler extends SpecialPage {
 		$out = $this->getOutput();
 
 		$out->addModuleStyles( 'mediawiki.special' );
-
-		$stats = $this->getStats();
-
-		foreach ( [ 'audios', 'videos' ] as $type ) {
-			// for grep timedmedia-audios, timedmedia-videos
-			$out->addHTML(
-				"<h2>"
-				. $this->msg( 'timedmedia-' . $type )->numParams( $stats[$type]['total'] )->escaped()
-				. "</h2>"
-			);
-			// Give grep a chance to find the usages: timedmedia-ogg-videos, timedmedia-webm-videos,
-			// timedmedia-ogg-audios, timedmedia-flac-audios, timedmedia-wav-audios
-			$formats = $type == 'audios' ? $this->audioFormats : $this->formats;
-			foreach ( $formats as $format => $condition ) {
-				if ( $stats[ $type ][ $format ] ) {
-					$out->addHTML(
-						$this->msg( "timedmedia-$format-$type" )->numParams( $stats[ $type ][ $format ] )->escaped()
-						. Html::element( 'br' )
-					);
-				}
-			}
-		}
 
 		$states = $this->getStates();
 		$this->renderState( $out, 'transcodes', $states, false );
@@ -150,39 +118,6 @@ class SpecialTimedMediaHandler extends SpecialPage {
 		}
 		$table .= '</table>';
 		return $table;
-	}
-
-	private function getStats() {
-		global $wgMemc;
-		$allTranscodes = WebVideoTranscode::enabledTranscodes();
-
-		$memcKey = wfMemcKey( 'TimedMediaHandler', 'stats', '1' /* version */ );
-		$stats = $wgMemc->get( $memcKey );
-		if ( !$stats ) {
-			$dbr = wfGetDB( DB_REPLICA );
-			$stats = [];
-			$stats[ 'videos' ] = [ 'total' => 0 ];
-			foreach ( $this->formats as $format => $condition ) {
-				$stats[ 'videos' ][ $format ] = (int)$dbr->selectField(
-					'image',
-					'COUNT(*)',
-					'img_media_type = "VIDEO" AND (' . $condition . ')',
-					__METHOD__
-				);
-				$stats[ 'videos' ][ 'total' ] += $stats[ 'videos' ][ $format ];
-			}
-			$stats[ 'audios' ] = [ 'total' => 0 ];
-			foreach ( $this->audioFormats as $format => $condition ) {
-				$stats[ 'audios' ][ $format ] = (int)$dbr->selectField(
-					'image',
-					'COUNT(*)',
-					'img_media_type = "AUDIO" AND (' . $condition . ')',
-					__METHOD__
-				);
-				$stats[ 'audios' ][ 'total' ] += $stats[ 'audios' ][ $format ];
-			}
-		}
-		return $stats;
 	}
 
 	private function getStates() {
