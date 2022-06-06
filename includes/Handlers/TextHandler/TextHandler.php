wikimedia/mediawiki-extensions-TimedMediaHandler

commit 12bf58eb18fae907effa180986532bdb4474e7b2

TextHandler::getTextTracksFromRows(): Remove unused code
Change-Id: I9f9702b099cde7f8938679ddebd37b727f1b84cf

includes/Handlers/TextHandler/TextHandler.php
@@ -14,7 +14,6 @@ use Exception;
 use File;
 use ForeignDBFile;
 use ForeignDBViaLBRepo;
-use ForeignTitle;
 use IForeignRepoWithMWApi;
 use Language;
 use LocalRepo;
@@ -263,20 +262,9 @@ class TextHandler {
 		$langNames = Language::fetchLanguageNames( null, 'mw' );
 
 		foreach ( $data as $row ) {
-			// Note, the namespace ID of this title might be 'unknown'
-			// to our configuration if this is called in ForeignDb situations
-			if ( $this->file->isLocal() ) {
-				$subTitle = Title::newFromRow( $row );
-			} else {
-				// @phan-suppress-next-next-line PhanTypeMismatchArgumentNullable $namespaceName is set
-				// @phan-suppress-next-line PhanPossiblyUndeclaredVariable $namespaceName is set
-				$subTitle = new ForeignTitle( $row->page_namespace, $namespaceName, $row->page_title );
-			}
 			$titleParts = explode( '.', $row->page_title );
 			if ( count( $titleParts ) >= 3 ) {
-				$timedTextExtension = array_pop( $titleParts );
 				$languageKey = array_pop( $titleParts );
-				$contentType = $this->getContentType( $timedTextExtension );
 			} else {
 				continue;
 			}

commit ac91caa2f56637e27f321c6e82d0d6da6c913698

Partial revert "TextHandler::getTextTracksFromRows(): Remove unused code"
This partially reverts commit 12bf58eb18fae907effa180986532bdb4474e7b2
which caused T309873.
While the variable was unused, the array_pop of course did manipulate
the array which we then later use.

Bug: T309873
Change-Id: I6d6414a635e64fd03901188f4b538b8da7712cf4

includes/Handlers/TextHandler/TextHandler.php
@@ -268,6 +268,7 @@ class TextHandler {
 		foreach ( $data as $row ) {
 			$titleParts = explode( '.', $row->page_title );
 			if ( count( $titleParts ) >= 3 ) {
+				$timedTextExtension = array_pop( $titleParts );
 				$languageKey = array_pop( $titleParts );
 			} else {
 				continue;