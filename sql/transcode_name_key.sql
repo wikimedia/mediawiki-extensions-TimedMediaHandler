7a15c8aa133bc6ebeac723e4458b2b95b29a9b24
Umherirrender <umherirrender_de.wp@web.de>
2022-06-10 23:48:15 +0200

    Bump minimum required version for upgrade to 1.31
    Remove sql patches, mention the gerrit change to check version
    - transcode_name_key.sql - I6358d5bec241f8fc57e5a04bad753e6fedf7fda1
    
    Change-Id: Ia9c1f200955fa183fc0a3bec35b795eef73eb1c4

--- sql/transcode_name_key.sql
+++
@@ -1,3 +0,0 @@
--- Add unique constrain on name/key
-DROP INDEX /*i*/transcode_name_inx ON /*_*/transcode;
-CREATE UNIQUE INDEX /*i*/transcode_name_key ON /*_*/transcode (transcode_image_name,transcode_key);
