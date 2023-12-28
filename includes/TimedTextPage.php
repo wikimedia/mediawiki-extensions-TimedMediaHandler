b4eb448484a59e85b88c405b0984446bf2c0fb6e
Derk-Jan Hartman <hartman.wiki@gmail.com>
2023-12-28 16:23:01 +0100

    TimedText: view improvements
    - Add a namespace tab to link back to the corresponding File page
    - Don't use the page's title for errors/warnings
    - Generate error for subtitle pages that do not have a corresponding
      file
    - Ignore redirects when looking up the corresponding file
    - Ignore non-TMH files when looking up the corresponding file
    - Hoist errors and warnings to the top of the page
    - Minor cleanup of styling
    
    A followup patch will rework the messages
    
    Bug: T333734
    Change-Id: I53ff4e7f266285ab44e3964a9ca0bbd47396fa66

--- includes/TimedTextPage.php
+++ includes/TimedTextPage.php
@@ -16,18 +16,18 @@ use HTMLForm;
 use LanguageCode;
 use MediaWiki\Html\Html;
 use MediaWiki\Languages\LanguageNameUtils;
-use MediaWiki\Linker\LinkTarget;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Output\OutputPage;
 use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Revision\SlotRecord;
+use MediaWiki\Status\Status;
 use MediaWiki\Title\Title;
-use Message;
+use StatusValue;
 use TextContent;
 
 class TimedTextPage extends Article {
 
-	/** @var int The width of the video plane. Must much the CSS */
+	/** @var int The width of the video plane. Must match the CSS */
 	private static $videoWidth = 400;
 
 	// WebVTT
@@ -47,11 +47,62 @@ class TimedTextPage extends Article {
 	 */
 	private $languageNameUtils;
 
+	/**
+	 * The file associated with this subtitle page
+	 * @var File|null
+	 */
+	private $correspondingFile;
+
+	/**
+	 * @var Title|null
+	 */
+	private $correspondingFileTitle;
+
+	/**
+	 * The TimedText format extracted from this page's title
+	 * @var null|string
+	 */
+	private $timedTextFormat;
+
+	/**
+	 * The language key extracted from this page's title
+	 * @var null|string
+	 */
+	private $languageKey;
+
+	/**
+	 * Status result of the view rendering
+	 *
+	 * @var StatusValue
+	 */
+	private StatusValue $renderStatus;
+
 	/** @inheritDoc */
 	public function __construct( Title $title, $oldId = null ) {
 		parent::__construct( $title, $oldId );
 		$services = MediaWikiServices::getInstance();
 		$this->languageNameUtils = $services->getLanguageNameUtils();
+
+		// We parse the title of this page to find the corresponding file's title
+		// in the File namespace, the language of the subtitles and the format.
+		$titleParts = explode( '.', $title->getDBkey() );
+		$timedTextExtension = array_slice( $titleParts, -1, 1 )[0] ?? null;
+
+		if ( in_array( $timedTextExtension, static::$knownTimedTextExtensions, true ) ) {
+			// File name with text extension ( from remaining parts of title )
+			// i.e TimedText:myfile.ogg.en.srt
+			$this->timedTextFormat = $timedTextExtension;
+			$this->languageKey = array_slice( $titleParts, -2, 1 )[0] ?? null;
+			$titleName = implode( '.', array_slice( $titleParts, 0, -2 ) );
+			$this->correspondingFileTitle = Title::newFromText( $titleName, NS_FILE );
+		} else {
+			// File name without text extension:
+			// i.e TimedText:myfile.ogg
+			$this->correspondingFileTitle = Title::newFromText( $this->getTitle()->getDBkey(), NS_FILE );
+			$this->timedTextFormat = null;
+			$this->languageKey = null;
+		}
+		$this->renderStatus = Status::newGood();
 	}
 
 	public function view(): void {
@@ -65,6 +116,7 @@ class TimedTextPage extends Article {
 			parent::view();
 			return;
 		}
+
 		// Article flag is required for some editors, and other features (T307218).
 		$out->setArticleFlag( true );
 
@@ -76,112 +128,97 @@ class TimedTextPage extends Article {
 
 	/**
 	 * Render TimedText to given output
+	 *
+	 * This function is used for views and diff views
+	 * It is somewhat special as it renders two separate units of content,
+	 * the timedtext and the corresponding file for that timedtext
+	 *
 	 * @param OutputPage $out
 	 */
 	public function renderOutput( OutputPage $out ): void {
-		// parse page title:
-		$titleParts = explode( '.', $this->getTitle()->getDBkey() );
-		$timedTextExtension = array_pop( $titleParts );
-		$languageKey = array_pop( $titleParts );
+		$this->renderStatus = Status::newGood();
 
-		$oldid = $this->getOldID();
-		// Are we looking at an old revision
-		if ( $oldid && $this->fetchRevisionRecord() ) {
-			$out->setRevisionId( $this->getRevIdFetched() );
-			$this->setOldSubtitle( $oldid );
-
-			if ( !$this->showDeletedRevisionHeader() ) {
-				wfDebug( __METHOD__ . ": cannot view deleted revision\n" );
-				return;
-			}
-		}
-
-		$repoGroup = MediaWikiServices::getInstance()->getRepoGroup();
-		// Check for File name without text extension:
+		// Check for the new/edit page title format
 		// i.e TimedText:myfile.ogg
-		$fileTitle = Title::newFromText( $this->getTitle()->getDBkey(), NS_FILE );
-		$file = $repoGroup->findFile( $fileTitle );
-		// Check for a valid srt page, present redirect form for the full title match:
-		if ( $file && $file->exists() &&
-			!in_array( $timedTextExtension, self::$knownTimedTextExtensions, true )
-		) {
-			if ( $file->isLocal() ) {
-				$this->doRedirectToPageForm();
-			} else {
-				$this->doLinkToRemote( $file );
-			}
+		$file = $this->getCorrespondingFile();
+		if ( $file && !$this->isActualTimedTextTitle() ) {
+			$this->doRedirectToPageForm( $out );
 			return;
 		}
 
-		// Check for File name with text extension ( from remaining parts of title )
-		// i.e TimedText:myfile.ogg.en.srt
+		// We want to render the contents of the page
 
-		$videoTitle = Title::newFromText( implode( '.', $titleParts ), NS_FILE );
-
-		// Check for remote file
-		$basefile = $repoGroup->findFile( $videoTitle );
-		if ( !$basefile ) {
-			$out->addHTML( wfMessage( 'timedmedia-subtitle-no-video' )->escaped() );
-			return;
-		}
-
-		if ( !$basefile->isLocal() ) {
-			$this->doLinkToRemote( $basefile );
-			return;
-		}
-
-		// Look up the language name:
+		// Look up the language name for the language that these subtitles use:
 		$language = $out->getLanguage()->getCode();
 		$languages = $this->languageNameUtils->getLanguageNames( $language, LanguageNameUtils::ALL );
-		$languageName = $languages[$languageKey] ?? $languageKey;
+		// @phan-suppress-next-line PhanTypeMismatchDimFetchNullable
+		$languageName = $languages[ $this->getLanguageKey() ] ?? $this->getLanguageKey();
 
-		// Set title
+		// Set title of the page
 		$message = $this->getPage()->exists() ?
 			'timedmedia-timedtext-title-edit-subtitles' :
 			'timedmedia-timedtext-title-create-subtitles';
-		$out->setPageTitleMsg( wfMessage( $message, $languageName, $videoTitle ) );
+		$out->setPageTitleMsg( wfMessage( $message, $languageName, $this->getCorrespondingFileTitle() ) );
 
+		// Attempt to render the content
+		$fileHtml = $this->getFileHTML();
+		$timedTextHtml = $this->getTimedTextHTML( $out, $languageName );
+
+		// Generate the page
+		$out->addHTML( $this->getErrorsAndWarnings( $this->renderStatus ) );
+		$out->addModuleStyles( [ 'ext.tmh.timedtextpage.styles' ] );
+
+		if ( !$this->renderStatus->isOK() ) {
+			return;
+		}
+
+		// Layout
 		$out->addHtml(
 			Html::rawElement( 'div', [ 'class' => 'mw-timedtextpage-layout' ],
 				Html::rawElement( 'div', [ 'class' => 'mw-timedtextpage-video' ],
-					$this->getVideoHTML( $videoTitle )
+					$fileHtml
 				) .
 				Html::rawElement(
 					'div',
 					[ 'class' => 'mw-timedtextpage-tt' ],
-					$this->getTimedTextHTML( $languageName )
+					$timedTextHtml
 				)
 			)
 		);
-		$out->addModuleStyles( [ 'ext.tmh.timedtextpage.styles' ] );
-
-		if ( !$oldid ) {
-			// Set wgRevision at the end from what we actually fetched.
-			$out->setRevisionId( $this->getRevIdFetched() );
-		}
 	}
 
 	/**
-	 * Timed text or file is hosted on remote repo, Add a short description and link to foring repo
-	 * @param File $file the base file
+	 * We show this form if a valid local file exists for this title.
+	 * i.e TimedText:myfile.ogg
+	 *
+	 * @return void
+	 * @throws \MWException
 	 */
-	private function doLinkToRemote( File $file ): void {
-		$output = $this->getContext()->getOutput();
-		$output->setPageTitleMsg( wfMessage( 'timedmedia-subtitle-remote',
-			$file->getRepo()->getDisplayName() ) );
-		$output->addHTML( wfMessage( 'timedmedia-subtitle-remote-link',
-			$file->getDescriptionUrl(), $file->getRepo()->getDisplayName() )->parse() );
-	}
-
-	private function doRedirectToPageForm(): void {
-		$context = $this->getContext();
+	private function doRedirectToPageForm( OutputPage $out ): void {
+		$context = $out->getContext();
 		$lang = $context->getLanguage();
-		$out = $context->getOutput();
+		$file = $this->getCorrespondingFile();
 
 		// Set the page title:
 		$out->setPageTitleMsg( wfMessage( 'timedmedia-subtitle-new' ) );
 
-		$out->enableOOUI();
+		if ( $file && !$file->isLocal() ) {
+			// Corresponding file is hosted on remote repo.
+			// People aren't really supposed to be here, so link to foreign repo
+			// TODO these two messages should be combined into a single one
+			$out->addHTML( Html::warningBox(
+				wfMessage( 'timedmedia-subtitle-remote',
+					$file->getRepo()->getDisplayName() )->parse(),
+				'' )
+			);
+			$out->addHTML( Html::warningBox(
+				wfMessage( 'timedmedia-subtitle-remote-link',
+					$file->getDescriptionUrl(),
+					$file->getRepo()->getDisplayName() )->parse(),
+				'' )
+			);
+			return;
+		}
 
 		$languages = $this->languageNameUtils->getLanguageNames(
 			LanguageNameUtils::AUTONYMS,
@@ -200,9 +237,10 @@ class TimedTextPage extends Article {
 				'type' => 'select',
 				'options' => $options,
 				'default' => $lang->getCode(),
-			]
+			],
 		];
 
+		$out->enableOOUI();
 		$htmlForm = HTMLForm::factory( 'ooui', $formDescriptor, $context );
 		$htmlForm
 			->setMethod( 'post' )
@@ -228,40 +266,81 @@ class TimedTextPage extends Article {
 		return false;
 	}
 
+	private function getErrorsAndWarnings( StatusValue $status ): string {
+		$results = [];
+		foreach ( $status->getErrorsByType( 'error' ) as $error ) {
+			$results[] = Html::errorBox( wfMessage( $error[ 'message' ], $error[ 'params' ] ) );
+		}
+		foreach ( $status->getErrorsByType( 'warning' ) as $error ) {
+			$results[] = Html::warningBox( wfMessage( $error[ 'message' ], $error[ 'params' ] ) );
+		}
+		return implode( "\n", $results );
+	}
+
 	/**
 	 * Gets the video HTML ( with the current language set as default )
-	 * @param LinkTarget $videoTitle
+	 *
 	 * @return string
 	 */
-	private function getVideoHTML( LinkTarget $videoTitle ): string {
+	private function getFileHTML(): string {
 		// Get the video embed:
-		$file = MediaWikiServices::getInstance()->getRepoGroup()->findFile( $videoTitle );
+		$file = $this->getCorrespondingFile();
 		if ( !$file ) {
-			return wfMessage( 'timedmedia-subtitle-no-video' )->escaped();
+			// TODO fix this message to be video AND audio
+			$this->renderStatus->error( 'timedmedia-subtitle-no-video' );
+			return '';
+		}
+		if ( !$file->isLocal() ) {
+			// File is hosted on remote repo, Add a short description and link to foreign repo
+			// TODO these two messages should be combined into a single one
+			$this->renderStatus->warning( 'timedmedia-subtitle-remote',
+				$file->getRepo()->getDisplayName() );
+			$this->renderStatus->warning( 'timedmedia-subtitle-remote-link',
+				$file->getDescriptionUrl(), $file->getRepo()->getDisplayName() );
+			return '';
+		}
+		if ( $this->getTitle()->isRedirect() ) {
+			return '';
 		}
 
-		$videoTransform = $file->transform(
-			[
-				'width' => self::$videoWidth
-			]
-		);
-		return $videoTransform->toHtml();
+		return $file->transform( [
+			'width' => self::$videoWidth
+		] )->toHtml();
 	}
 
 	/**
 	 * Gets an HTML representation of the Timed Text
 	 *
+	 * @param OutputPage $out
 	 * @param string $languageName
-	 * @return Message|string
+	 * @return string
 	 */
-	private function getTimedTextHTML( string $languageName ) {
+	private function getTimedTextHTML( OutputPage $out, string $languageName ) {
+		$file = $this->getCorrespondingFile();
 		if ( !$this->getPage()->exists() ) {
-			return wfMessage( 'timedmedia-subtitle-no-subtitles', $languageName );
+			if ( $file && $file->isLocal() ) {
+				$this->renderStatus->warning( 'timedmedia-subtitle-no-subtitles' );
+			}
+			return '';
 		}
+		if ( $file && !$file->isLocal() ) {
+			// There are local subtitles for remote file, which doesn't work
+			$this->renderStatus->error( 'timedmedia-subtitle-no-video' );
+		}
+		$oldid = $this->getOldID();
+		// Are we looking at an old revision
+		if ( $oldid && $this->fetchRevisionRecord() ) {
+			$out->setRevisionId( $this->getRevIdFetched() );
+			$this->setOldSubtitle( $oldid );
 
+			if ( !$this->showDeletedRevisionHeader() ) {
+				wfDebug( __METHOD__ . ": cannot view deleted revision\n" );
+			}
+		}
 		$revision = $this->fetchRevisionRecord();
 		if ( !$revision ) {
-			return wfMessage( 'noarticletext', $languageName );
+			$this->renderStatus->fatal( 'noarticletext' );
+			return '';
 		}
 
 		$content = $revision->getContent(
@@ -270,13 +349,81 @@ class TimedTextPage extends Article {
 			$this->getContext()->getUser()
 		);
 		if ( !$content ) {
-			return wfMessage( 'rev-deleted-text-permission', $languageName );
+			$this->renderStatus->fatal( 'rev-deleted-text-permission', $languageName );
+			return '';
 		}
 
+		if ( !$oldid ) {
+			// Set wgRevision at the end from what we actually fetched.
+			$out->setRevisionId( $this->getRevIdFetched() );
+		}
 		return Html::element(
 			'pre',
 			[],
-			( $content instanceof TextContent ) ? $content->getText() : null
+			( $content instanceof TextContent ) ? $content->getText() : ''
 		);
 	}
+
+	/**
+	 * Retrieve the file associated with this TimedText page
+	 * Returns null if no file is associated or no file exists,
+	 * either locally or on a remote server or if it is not a TimedMediaHandler file
+	 *
+	 * @return File|null
+	 */
+	public function getCorrespondingFile(): ?File {
+		if ( $this->correspondingFile ) {
+			return $this->correspondingFile;
+		}
+
+		$repoGroup = MediaWikiServices::getInstance()->getRepoGroup();
+		$fileTitle = $this->getCorrespondingFileTitle();
+		if ( !$fileTitle ) {
+			return null;
+		}
+		$file = $repoGroup->findFile( $fileTitle, [ 'ignoreRedirects' => true ] );
+
+		if ( $file && $file->exists() && $file->getHandler() instanceof TimedMediaHandler ) {
+			$this->correspondingFile = $file;
+			return $this->correspondingFile;
+		}
+		return null;
+	}
+
+	/**
+	 * The media file title that should belong to this TimedText page
+	 *
+	 * The title doesn't necessarily have to exist
+	 * @return Title|null
+	 */
+	public function getCorrespondingFileTitle(): ?Title {
+		return $this->correspondingFileTitle;
+	}
+
+	/**
+	 * Returns the extension/timedtext type, based on the page title
+	 * @return string|null
+	 */
+	public function getTimedTextFormat(): ?string {
+		return $this->timedTextFormat;
+	}
+
+	/**
+	 * Only pages that end with .languageKey.srt
+	 * are known allowed names for TimedText pages.
+	 *
+	 * @return bool
+	 */
+	public function isActualTimedTextTitle(): bool {
+		return (bool)$this->getTimedTextFormat();
+	}
+
+	/**
+	 * Returns the language key code from the page title, if present
+	 * @return string|null
+	 */
+	public function getLanguageKey(): ?string {
+		return $this->languageKey;
+	}
+
 }
