a3792c8d19e403f16f814aafb50409337fa5c3ee
Ed Sanders <esanders@wikimedia.org>
2024-06-10 18:02:28 +0100

    JSDoc: Use externals plugin
    Change-Id: I776c95e10ccb7751fc6f825a77545a5d15cb1844

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,8 @@ module.exports = function ( grunt ) {
 	grunt.initConfig( {
 		eslint: {
 			options: {
-				cache: true
+				cache: true,
+				fix: grunt.option( 'fix' )
 			},
 			all: '.'
 		},
--- resources/ext.tmh.player.element.js
+++ resources/ext.tmh.player.element.js
@@ -199,7 +199,7 @@ class MediaElement {
 	 * Key press handler for `<a role="button">` element to open a
 	 * dialog and play a {MediaElement}.
 	 *
-	 * @param {KeyBoardEvent} event
+	 * @param {KeyboardEvent} event
 	 */
 	keyPressHandler( event ) {
 		if (
@@ -325,7 +325,7 @@ MediaElement.currentlyPlaying = false;
  * There should be only 1 interstitial to indicate the dialog is loading.
  *
  * @static
- * @type {JQuery?}
+ * @type {jQuery?}
  */
 MediaElement.$interstitial = null;
 
