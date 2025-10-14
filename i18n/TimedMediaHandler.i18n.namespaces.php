f129ac92728e4cc1af08fe6918c79d9bdbd14c7f
Jon Harald Søby <jhsoby@gmail.com>
2025-10-14 10:39:23 +0200

    Add namespace translations for TimedMediaHandler
    Add requested namespace translations for TimedMediaHandler,
    now that it supports namespace localization. In addition to the
    ones requested, also adding translations for my native
    Norwegian.
    
    [Because fallback chains are not handled, added a manual
    linkage from 'no' if anybody tries to use the old generic
    code which will fall back to 'nb' elsewhere in MediaWiki
    i18n stuff. -bv]
    
    Bug: T315728
    Bug: T373239
    Bug: T315715
    Bug: T122127
    Bug: T391946
    Bug: T406997
    Change-Id: I1e6b1de17eab284bd5c435548be989ee099786b0

--- i18n/TimedMediaHandler.i18n.namespaces.php
+++ i18n/TimedMediaHandler.i18n.namespaces.php
@@ -15,3 +15,61 @@ $wgTimedMediaHandlerNamespaceNames['en'] = [
 	'timedtext'      => 'TimedText',
 	'timedtext_talk' => 'TimedText_talk',
 ];
+
+// Be aware that fallback languages are not processed in the setup
+// of these namespaces, because we can't safely load the fallback lists
+// yet at namespace setup time.
+//
+// Add entries for all languages that need to use the fallbacks
+
+// T373239
+$wgTimedMediaHandlerNamespaceNames['ary'] = [
+	'timedtext'      => 'سوتيتر',
+	'timedtext_talk' => 'لمداكرة_د_سوتيتر',
+];
+
+// T122127
+$wgTimedMediaHandlerNamespaceNames['glk'] = [
+	'timedtext'      => 'همزمت_وؤت',
+	'timedtext_talk' => 'همزمت_وؤتˇ_گب',
+];
+
+// T315715
+$wgTimedMediaHandlerNamespaceNames['is'] = [
+	'timedtext'      => 'Skjátexti',
+	'timedtext_talk' => 'Skjátextaspjall',
+];
+
+$wgTimedMediaHandlerNamespaceNames['nb'] = [
+	'timedtext'      => 'Undertekster',
+	'timedtext_talk' => 'Undertekstdiskusjon',
+];
+
+$wgTimedMediaHandlerNamespaceAliases['nb'] = [
+	'timedtext' => [ 'Undertekst' ],
+];
+
+$wgTimedMediaHandlerNamespaceNames['nn'] = [
+	'timedtext'      => 'Undertekstar',
+	'timedtext_talk' => 'Undertekstdiskusjon',
+];
+
+$wgTimedMediaHandlerNamespaceAliases['nn'] = [
+	'timedtext' => [ 'Undertekst' ],
+];
+
+// Fallback chains are not handled automatically
+$wgTimedMediaHandlerNamespaceNames['no'] = $wgTimedMediaHandlerNamespaceNames['nb'];
+$wgTimedMediaHandlerNamespaceAliases['no'] = $wgTimedMediaHandlerNamespaceAliases['nb'];
+
+// T406997
+$wgTimedMediaHandlerNamespaceNames['ps'] = [
+	'timedtext'      => 'مهال‌ليک',
+	'timedtext_talk' => 'د مهال‌ليک خبرې اترې',
+];
+
+// T391946
+$wgTimedMediaHandlerNamespaceNames['uk'] = [
+	'timedtext'      => 'Субтитри',
+	'timedtext_talk' => 'Обговорення_субтитрів',
+];
